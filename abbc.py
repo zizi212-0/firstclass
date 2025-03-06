68b0a75f794592aa3f72aeef9a6a10c5dc99df32
Bidhi <bidhikc12@gmail.com>
2025-03-06 23:12:13 +0545

    message

--- abbc.py
+++ abbc.py
@@ -260,33 +260,35 @@
 # for y in x.values():
 #     print(y)
 
-class Car:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-    def move(self):
-        print ("Drive!")
-class Boat:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-    def move(self):
-        print ("Sail!")
-class Plane:
-    def __init__(self, brand, model):
-        self.brand = brand
-        self.model = model
-    def move(self):
-        print ("Fly!")
+# class Car:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+#     def move(self):
+#         print ("Drive!")
+# class Boat:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+#     def move(self):
+#         print ("Sail!")
+# class Plane:
+#     def __init__(self, brand, model):
+#         self.brand = brand
+#         self.model = model
+#     def move(self):
+#         print ("Fly!")
 
-car1= Car("ford", "mustang")
-boat1=Boat("Ibiza", "Touring 20")
-plane1=Plane("boeing", "747")
+# car1= Car("ford", "mustang")
+# boat1=Boat("Ibiza", "Touring 20")
+# plane1=Plane("boeing", "747")
 
-for x in (car1, boat1, plane1):
-    x.move() #only calling one time 
+# for x in (car1, boat1, plane1):
+#     x.move() #only calling one time 
 
-    
+
+
+
 
     
 
